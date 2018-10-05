7f6976ea1d7f5a8c3a47287e2277f703a9ced4de
yufong1267 <a2583669@gmail.com>
2018-10-05 15:30:35 +0800

    C#Game_Hamster
    In the project using visual studio to make it.
    And it's anothe pratice for using timer.

---
+++ HitRatGameplay/HitRatGameplay/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace HitRatGameplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Apress = new System.Windows.Forms.Button();
+            this.Spress = new System.Windows.Forms.Button();
+            this.Dpress = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Timeleft = new System.Windows.Forms.Label();
+            this.animation_timer = new System.Windows.Forms.Timer(this.components);
+            this.test = new System.Windows.Forms.Label();
+            this.Quit = new System.Windows.Forms.Button();
+            this.killrat_timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Apress
+            // 
+            this.Apress.Image = global::HitRatGameplay.Properties.Resources.花路米;
+            this.Apress.Location = new System.Drawing.Point(12, 384);
+            this.Apress.Name = "Apress";
+            this.Apress.Size = new System.Drawing.Size(104, 57);
+            this.Apress.TabIndex = 0;
+            this.Apress.Text = "按A";
+            this.Apress.UseVisualStyleBackColor = true;
+            this.Apress.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Spress
+            // 
+            this.Spress.Location = new System.Drawing.Point(134, 384);
+            this.Spress.Name = "Spress";
+            this.Spress.Size = new System.Drawing.Size(85, 57);
+            this.Spress.TabIndex = 1;
+            this.Spress.Text = "按S";
+            this.Spress.UseVisualStyleBackColor = true;
+            this.Spress.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Dpress
+            // 
+            this.Dpress.Location = new System.Drawing.Point(249, 384);
+            this.Dpress.Name = "Dpress";
+            this.Dpress.Size = new System.Drawing.Size(91, 57);
+            this.Dpress.TabIndex = 2;
+            this.Dpress.Text = "按D";
+            this.Dpress.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Dpress.UseVisualStyleBackColor = true;
+            this.Dpress.Click += new System.EventHandler(this.Dpress_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Timeleft
+            // 
+            this.Timeleft.AutoSize = true;
+            this.Timeleft.Location = new System.Drawing.Point(667, 20);
+            this.Timeleft.Name = "Timeleft";
+            this.Timeleft.Size = new System.Drawing.Size(39, 15);
+            this.Timeleft.TabIndex = 3;
+            this.Timeleft.Text = "30:00";
+            // 
+            // animation_timer
+            // 
+            this.animation_timer.Tick += new System.EventHandler(this.animation_timer_Tick);
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Location = new System.Drawing.Point(667, 109);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(0, 15);
+            this.test.TabIndex = 4;
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(848, 418);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(75, 23);
+            this.Quit.TabIndex = 5;
+            this.Quit.Text = "結束";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // killrat_timer
+            // 
+            this.killrat_timer.Tick += new System.EventHandler(this.killrat_timer_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(960, 479);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.Timeleft);
+            this.Controls.Add(this.Dpress);
+            this.Controls.Add(this.Spress);
+            this.Controls.Add(this.Apress);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "sdaasaddsadsaadsadddsaads";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Apress;
+        private System.Windows.Forms.Button Spress;
+        private System.Windows.Forms.Button Dpress;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Timeleft;
+        private System.Windows.Forms.Timer animation_timer;
+        private System.Windows.Forms.Label test;
+        private System.Windows.Forms.Button Quit;
+        private System.Windows.Forms.Timer killrat_timer;
+    }
+}
+
---
+++ HitRatGameplay/HitRatGameplay/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices; //调用WINDOWS API函数时要用到
+using Microsoft.Win32; //写入注册表时要用到
+using System;
+using System.Threading;
+using System.Drawing.Drawing2D;
+
+namespace HitRatGameplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        
+        private int left_time = 3000;
+
+        private char ran_rat = 'A'; //default
+        private int Final_Point = 0;
+        //定義遊戲按鍵
+        private char[] sign = new char[] { 'A' , 'S' , 'D' };
+        private Random rnd = null;
+        //定義rat
+        private Button rat = null;
+        private void Form1_Load(object sender, EventArgs e )
+        {
+            
+            this.Text = "打地鼠";
+            Timeleft.Size = new Size(100, 50);
+            //建立亂數
+            Random reg  = new Random(Guid.NewGuid().GetHashCode());
+            rnd = reg;
+
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+
+            animation_timer = new System.Windows.Forms.Timer();
+            animation_timer.Interval = 1;
+            animation_timer.Tick += new EventHandler(animation_timer_Tick);
+
+            killrat_timer = new System.Windows.Forms.Timer();
+            killrat_timer.Interval = 1;
+            killrat_timer.Tick += new EventHandler(killrat_timer_Tick);
+        }
+
+        private bool rat_to_bottom = false;
+        private void btn_Click(object sender, EventArgs e)
+        {
+            //確認按下A 這邊做 確認random = 1 確認後random要做更換圖片(往下移/往上移)
+               if(ran_rat == 'A')
+                {
+                
+                animation_timer.Enabled = false;
+                killrat_timer.Enabled = true;
+                if(rat_to_bottom)
+                {
+                    rat.Dispose();
+                    this.Controls.Remove(rat);
+                    rat_alive = false;
+                    Final_Point++;
+                    rat_to_bottom = false;
+                    killrat_timer.Enabled = false;
+                }
+                }
+        }
+
+        private bool pressed = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && !pressed)
+            {
+                btn_Click(null, null);
+                pressed = true;
+            }
+            if (e.KeyCode == Keys.S && !pressed)
+            {
+                button2_Click(null, null);
+                pressed = true;
+            }
+            if (e.KeyCode == Keys.D && !pressed)
+            {
+                Dpress_Click(null, null);
+                pressed = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //這邊按S
+            //這邊做 確認random 確認後random = 2要做更換圖片(往下移/往上移)
+            if (ran_rat == 'S')
+            {
+                
+                animation_timer.Enabled = false;
+                killrat_timer.Enabled = true;
+                //這邊放animation
+                if(rat_to_bottom)
+                {
+                    rat.Dispose();
+                    this.Controls.Remove(rat);
+                    rat_alive = false;
+                    Final_Point++;
+                    rat_to_bottom = false;
+                    killrat_timer.Enabled = false;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //這邊解除已經按下
+            pressed = false;
+        }
+
+        private void Dpress_Click(object sender, EventArgs e)
+        {
+            //確認按下的"D" 確認random 確認後random = 3要做更換圖片(往下移/往上移)
+            if (ran_rat == 'D')
+            {
+                
+                animation_timer.Enabled = false;
+                killrat_timer.Enabled = true;
+                if(rat_to_bottom)
+                {
+                    rat.Dispose();
+                    this.Controls.Remove(rat);
+                    rat_alive = false;
+                    Final_Point++;
+                    rat_to_bottom = false;
+                    killrat_timer.Enabled = false;
+                }  
+            }
+        }
+
+        private bool rat_alive = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (left_time == 1)
+            {
+                timer1.Stop();
+                MessageBox.Show("最後的分數是" + Final_Point);
+                Application.Restart();
+            }
+            left_time--;
+            Timeleft.Text = "剩下：" + left_time / 100 + "." + left_time % 100 + "\n 現在的分數是：" + Final_Point;
+
+            if (left_time % 50 == 0 && !rat_alive)
+            {
+                rat_alive = true;
+                ran_rat = sign[rnd.Next(0,3)];
+                //這邊生成1隻地鼠
+                Button reg_btn = new Button();
+                reg_btn.Size = new Size(75, 23);
+                reg_btn.Text = "我是地鼠";
+                    
+                switch(ran_rat)
+                {
+                    case 'A':
+                        reg_btn.Location = new Point(Apress.Location.X, Apress.Location.Y - 23);
+                        break;
+                    case 'S':
+                        reg_btn.Location = new Point(Spress.Location.X, Spress.Location.Y - 23);
+                        break;
+                    case 'D':
+                        reg_btn.Location = new Point(Dpress.Location.X, Dpress.Location.Y - 23);
+                        break;
+                }
+                rat = reg_btn;
+                this.Controls.Add(rat);
+                killrat_timer.Enabled = false;
+                animation_timer.Enabled = true;
+                delay_time = 46;
+            }
+
+        }
+
+        private int delay_time = 46;
+        private void animation_timer_Tick(object sender, EventArgs e)
+        {
+            //do nothing just for delay
+            delay_time--; 
+              
+            rat.Top--;
+            if(rat.Top <= 316)
+            {
+                animation_timer.Enabled = false;
+                rat_to_bottom = false;
+            }
+            if(delay_time == 1)
+            {
+                animation_timer.Stop();
+            }
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        private void killrat_timer_Tick(object sender, EventArgs e)
+        {
+            delay_time++;
+            rat.Top++;
+            if (rat.Top >= 350)
+            {
+                animation_timer.Enabled = false;
+                rat_to_bottom = true;
+                switch (ran_rat)
+                {
+                    case 'A':
+                        btn_Click(null, null);
+                        break;
+                    case 'S':
+                        button2_Click(null, null);
+                        break;
+                    case 'D':
+                        Dpress_Click(null, null);
+                        break;
+                }
+            }
+
+            if (delay_time == 46)
+            {
+                animation_timer.Stop();
+            }
+        }
+    }
+}
